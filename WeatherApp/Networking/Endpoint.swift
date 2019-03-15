9a278900f7b2dbd951de7ab63b78468bf62911c0
Piotr Olech <piotr@Piotrs-MacBook-Pro.local>
2019-03-15 16:55:01 +0100

    Add http client and endpoint class

---
+++ WeatherApp/Networking/Endpoint.swift
@@ -0,0 +1,48 @@
+//
+//  Endpoint.swift
+//  WeatherApp
+//
+//  Created by Piotr Olech on 15/03/2019.
+//  Copyright © 2019 Piotr Olech. All rights reserved.
+//
+
+import Alamofire
+
+final class Endpoint<Response> {
+    let path: String
+    let method: HTTPMethod
+    let parameters: Parameters?
+    let encoding: ParameterEncoding
+    let headers: HTTPHeaders?
+    let decode: (Data) throws -> Response
+    
+    init(path: String,
+         method: HTTPMethod = .get,
+         parameters: Parameters? = nil,
+         encoding: ParameterEncoding = URLEncoding.default,
+         headers: HTTPHeaders? = nil,
+         decode: @escaping (Data) throws -> Response) {
+        self.path = path
+        self.method = method
+        self.parameters = parameters
+        self.encoding = encoding
+        self.headers = headers
+        self.decode = decode
+    }
+}
+
+// MARK: - Convenience init
+
+extension Endpoint where Response: Decodable {
+    convenience init(method: HTTPMethod = .get,
+                     path: String,
+                     parameters: Parameters? = nil,
+                     encoding: ParameterEncoding = URLEncoding.default,
+                     headers: HTTPHeaders? = nil) {
+        self.init(path: path, method: method, parameters: parameters, encoding: encoding, headers: headers) {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(Response.self, from: $0)
+        }
+    }
+}
